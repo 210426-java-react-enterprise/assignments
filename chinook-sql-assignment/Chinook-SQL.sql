4f34253ec9c466d99213349997258c82cfb183e9
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-05-05 14:12:14 -0700

    Added some comments to some previous commands following some group discussions

--- chinook-sql-assignment/Chinook-SQL.sql
+++ chinook-sql-assignment/Chinook-SQL.sql
@@ -4,6 +4,8 @@
 --2.1 select
 select * from "Employee";
 select * from "Employee" where "LastName" = 'King';
+
+--the is keyword is required for NULL checks
 select * from "Employee" where "FirstName" = 'Andrew' and "ReportsTo" is NULL;
 
 --2.2 Order By
@@ -11,6 +13,7 @@ select * from "Album" order by "Title" desc;
 select "FirstName" from "Employee" order by "FirstName" asc;
 
 --2.3 Insert into
+--Had to add id manually due to the ID being non-null. I think the table isn't serializable as well
 insert into "Genre" values (26, 'Techno'), (27, 'Kpop');
 insert into "Employee" values (11, 'Barack', 'Obama'), (10, 'Lincoln', 'Abraham');
 insert into "Customer" values (60, 'Giancarlo', 'Tomasello', 'Revature', '123 California Way', 'San Jose', 'CA', 'USA', null, null, null, 'gtomasel@mail.com'), 
