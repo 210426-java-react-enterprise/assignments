41655b339e55e3c053655727eac0a725ce6ff7c3
ozzy castillo <oswaldo.castillo@revature.net>
2021-05-07 19:11:20 -0500

    Looking for solution to SQLException threw by DriverManager in ConnectionFactory

--- src/main/java/com/revature/assigments/p0/util/AppState.java
+++ src/main/java/com/revature/assigments/p0/util/AppState.java
@@ -1,7 +1,9 @@
 package com.revature.assigments.p0.util;
 
+import com.revature.assigments.p0.daos.UserDAO;
 import com.revature.assigments.p0.screens.LandingScreen;
 import com.revature.assigments.p0.screens.SignUpScreen;
+import com.revature.assigments.p0.services.UserService;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -18,9 +20,12 @@ public class AppState {
         appRunning = true;
         consoleReader = new BufferedReader(new InputStreamReader(System.in));
 
+        final UserDAO userDAO = new UserDAO();
+        final UserService userService = new UserService(userDAO);
+
         router = new ScreenRouter();
         router.addScreen(new LandingScreen(consoleReader,router))
-                .addScreen(new SignUpScreen(consoleReader,router)); // Here I can add more screens
+                .addScreen(new SignUpScreen(consoleReader, userService)); // Here I can add more screens
 
         System.out.println("Application initialized!");
     }
