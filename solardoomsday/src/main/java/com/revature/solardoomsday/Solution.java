0a9ff2a3d5808cde0164920b3ed4713afda5f5dc
SoftWarlus <urosv16@gmail.com>
2021-06-18 17:54:23 -0400

    test commit

---
+++ solardoomsday/src/main/java/com/revature/solardoomsday/Solution.java
@@ -0,0 +1,34 @@
+package com.revature.solardoomsday;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader bf = new BufferedReader(new FileReader("src/main/resources/test.txt"));
+			String input = bf.readLine();
+			while(input != null) {
+				System.out.println(input);
+				input = bf.readLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	static class Result {
+
+
+		public static List<Integer> calculateDoomsday(int area) {
+
+		}
+	}
+}
+
+
