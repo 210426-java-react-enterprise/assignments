c964e9f44398af69a1f88f327014e5bac408e3bc
ChrisLevano <83236704+ChrisLevano@users.noreply.github.com>
2021-06-18 17:51:59 -0400

    test cases

---
+++ Solar-cl/src/main/java/SolarDoomsday.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.List;
+
+public class SolarDoomsday {
+    int areas[] = {12, 15324, 47};
+
+    (int i = 0; i < 3; i++){
+    int area = areas[i];
+        solution(area);
+    }
+
+    public Array solution(int area){
+
+    }
+}
