3824e9cac446aedfd5aafd3194707439888920aa
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-06-21 09:21:59 -0700

    Created the test cases for the coding challenge

---
+++ SolarDoomsday/src/main/java/SolarDoomdayProblem.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SolarDoomdayProblem {
+
+    public List<Integer> solution(int area){
+        List<Integer> tiles = new ArrayList<>();
+
+
+
+        return tiles;
+    }
+}
---
+++ SolarDoomsday/src/test/java/SolutionTest.java
@@ -0,0 +1,44 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SolutionTest {
+
+    private SolarDoomdayProblem sut;
+
+    @Before
+    public void setUp(){
+        sut = new SolarDoomdayProblem();
+    }
+
+    @After
+    public void tearDown(){
+        sut = null;
+    }
+
+    @Test
+    public void test_case1(){
+        List<Integer> expected = Arrays.asList(9, 1,1,1);
+
+        List<Integer> actual = new ArrayList<>();
+        actual = sut.solution(12);
+
+        Assert.assertSame(actual, expected);
+    }
+
+    @Test
+    public void test_case2(){
+        List<Integer> expected = Arrays.asList(15129, 169,25,1);
+
+        List<Integer> actual = new ArrayList<>();
+        actual = sut.solution(15324);
+
+        Assert.assertSame(actual, expected);
+    }
+
+}
