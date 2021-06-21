f1e34069f49953b47bba7a59f87597514090630a
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-06-21 09:46:12 -0700

    Solved the test cases but need to fix the assert

--- SolarDoomsday/src/main/java/SolarDoomdayProblem.java
+++ SolarDoomsday/src/main/java/SolarDoomdayProblem.java
@@ -6,7 +6,13 @@ public class SolarDoomdayProblem {
     public List<Integer> solution(int area){
         List<Integer> tiles = new ArrayList<>();
 
+        while(area > 0){
+            int t = (int) Math.sqrt(area);
+            t *= t;
 
+            tiles.add(t);
+            area -= t;
+        }
 
         return tiles;
     }
