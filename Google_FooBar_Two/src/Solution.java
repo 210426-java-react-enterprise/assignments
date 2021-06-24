5c5f0a48c61dba8b1ea3589a775a86df1ccff3b3
jbialon32 <jbialon32@gmail.com>
2021-06-24 11:35:56 -0500

    progress

--- Google_FooBar_Two/src/Solution.java
+++ Google_FooBar_Two/src/Solution.java
@@ -1,4 +1,6 @@
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,11 +13,32 @@ public class Solution {
 
     public Solution() {super();}
 
-    public int solution(List integerList) {
+    public int solution(List<Integer> integerList) {
 
         int returnVal = 0;
+        String intStr = "";
 
-        return returnVal;
+        integerList.sort(Comparator.reverseOrder());
+
+        System.out.println(integerList);
+
+        List<String> strList = integerList.stream()
+                .map(i -> String.valueOf(i))
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < strList.size(); i++) {
+            intStr += strList.get(i);
+        }
+
+        returnVal = Integer.parseInt(intStr);
+
+        if (returnVal % 3 == 0) {
+            return returnVal;
+        }
+
+        System.out.println("9".compareTo("1"));
+
+        return 0;
+
     }
-
 }
