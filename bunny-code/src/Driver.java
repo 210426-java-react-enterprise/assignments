60a903df7b2d2b34d5fa5eaab30f8479aca32473
DanceableBleats <KylePlummerBSCS@gmail.com>
2021-06-24 14:34:05 -0400

    added timer

--- bunny-code/src/Driver.java
+++ bunny-code/src/Driver.java
@@ -1,6 +1,9 @@
+import java.sql.Time;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Timer;
 
 public class Driver {
     public static void main(String[] args){
@@ -11,9 +14,13 @@ public class Driver {
         testList.add(Arrays.asList(3, 1, 4, 1, 5, 9));
         testList.add(Arrays.asList(4, 4, 1, 1, 1, 3));
 
+        Long totalTimeStart = System.currentTimeMillis();
+
         Solution solution = new Solution();
         for (List<Integer> plates : testList) {
-            System.out.println("SOLUTION {" + plates + "}: " + solution.solution(plates));
+            Long start = System.currentTimeMillis();
+            System.out.println("SOLUTION {" + plates + "}: " + solution.solution(plates) + " MS: " + (System.currentTimeMillis() - start));
         }
+        System.out.println("Total Time (MS): " + (System.currentTimeMillis() - totalTimeStart));
     }
 }
